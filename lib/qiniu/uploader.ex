6e9f15ce6080c42a50edc71801e3c06860bc26db
keroro520 <guozhen.wang@upai.com>
2017-06-20 14:52:26 +0800

    feat(uploader): support upload data

--- lib/qiniu/uploader.ex
+++ lib/qiniu/uploader.ex
@@ -30,18 +30,25 @@ defmodule Qiniu.Uploader do
     * `:crc32` - crc32 to check the file
     * `others` - Custom fields `atom: "string"`, e.g. `foo: "foo", bar: "bar"`
   """
-  def upload(put_policy, local_file, opts \\ [])
 
-  def upload(%PutPolicy{}=put_policy, local_file, opts) do
+  def upload(put_policy_or_token, local_file_or_data, opts \\ [])
+
+  def upload(%PutPolicy{}=put_policy, local_file_or_data, opts) do
     uptoken = Qiniu.Auth.generate_uptoken(put_policy)
-    upload(uptoken, local_file, opts)
-  end
 
-  def upload(uptoken, local_file, opts) when is_binary(uptoken) do
     # https://github.com/benoitc/hackney#send-a-body
     # Name should be string
     opts = Enum.map(opts, fn {k, v} -> {to_string(k), to_string(v)} end)
+    upload(uptoken, local_file_or_data, opts)
+  end
+  def upload(uptoken, {:local_file, local_file}, opts) when is_binary(uptoken) do
     data = List.flatten opts, [{:file, local_file}, {"token", uptoken}]
+    post_data = {:multipart, data}
+
+    Qiniu.HTTP.post(Qiniu.config[:up_host], post_data)
+  end
+  def upload(uptoken, {:data, data}, opts) when is_binary(uptoken) do
+    data = List.flatten opts, [{"file", data}, {"token", uptoken}]
     post_data = {:multipart, data}
 
     Qiniu.HTTP.post(Qiniu.config[:up_host], post_data)
